85662f1d9dd2012c905debb888dbdd96eee869b5
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-06-20 22:42:04 +0530

    Loader Component

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,6 +6,7 @@ import FormContainer from "../components/FormContainer";
 import { useLoginMutation } from "../Slices/usersApiSlice";
 import { setCredentials } from "../Slices/authSlice";
 import { toast } from "react-toastify";
+import Loader from "../components/Loader";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState();
@@ -39,28 +40,31 @@ const LoginScreen = () => {
     <FormContainer>
       <h1>Sign In</h1>
       <Form onSubmit={submitHandler}>
-        <Form.Group className="my-2" controlId="email">
+        <Form.Group className='my-2' controlId='email'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-            type="email"
-            placeholder="Enter Email"
+            type='email'
+            placeholder='Enter Email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        <Form.Group className="my-2" controlId="password">
+        <Form.Group className='my-2' controlId='password'>
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="Enter Password"
+            type='password'
+            placeholder='Enter Password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        <Button type="submit" variant="primary" className="mt-3">
+
+        {isLoading && <Loader />}
+
+        <Button type='submit' variant='primary' className='mt-3'>
           Sign In
         </Button>
-        <Row className="py-3">
+        <Row className='py-3'>
           <Col>
             New Customer?<Link to={"/register"}>Register</Link>
           </Col>
